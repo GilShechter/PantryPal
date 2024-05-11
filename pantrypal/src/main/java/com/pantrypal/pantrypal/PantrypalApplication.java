42949350713acbe0dda99afa075f79c2d8d9b0bc
Gil Shechter <gilshechter13@gmail.com>
2024-05-11 17:10:50 +0300

    With post method to identifier

--- pantrypal/src/main/java/com/pantrypal/pantrypal/PantrypalApplication.java
+++ pantrypal/src/main/java/com/pantrypal/pantrypal/PantrypalApplication.java
@@ -2,6 +2,8 @@ package com.pantrypal.pantrypal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PantrypalApplication {
@@ -10,4 +12,8 @@ public class PantrypalApplication {
 		SpringApplication.run(PantrypalApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- pantrypal/src/main/java/com/pantrypal/pantrypal/controller/IdentifierController.java
+++ pantrypal/src/main/java/com/pantrypal/pantrypal/controller/IdentifierController.java
@@ -1,17 +1,35 @@
 package com.pantrypal.pantrypal.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/identifier")
 public class IdentifierController {
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public ResponseEntity<?> hello()
-    {
-        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+
+    @Value("${api.target.url}")
+    private String targetUrl;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> identify(@RequestParam String imageUrl) throws UnsupportedEncodingException {
+        try{
+            String url = targetUrl + imageUrl;
+            ResponseEntity<String> response = restTemplate.postForEntity(url, null, String.class);
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        } catch (Exception e) {
+            // Handle exceptions, possibly returning an appropriate HTTP status
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " + e.getMessage());
+        }
     }
 }
