1a6afee5d580cc58888ae19302a4a2c0455ac9c3
Gil Shechter <gilshechter13@gmail.com>
2024-06-10 13:30:25 +0300

    With logout button

--- pantrypal/src/main/resources/static/js/home.js
+++ pantrypal/src/main/resources/static/js/home.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const token = localStorage.getItem('token');
+    const logoutBtn = document.getElementById('logoutBtn');
+
+    logoutBtn.addEventListener('click', () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userId');
+        window.location.href = '/home';
+    });
 
     if (token) {
         // User is authenticated
         document.getElementById('likedNavItem').style.display = 'block';
         document.getElementById('historyNavItem').style.display = 'block';
         document.getElementById('loginNavItem').style.display = 'none';
+        document.getElementById('logoutNavItem').style.display = 'block';
     } else {
         // User is not authenticated
         document.getElementById('likedNavItem').style.display = 'none';
         document.getElementById('historyNavItem').style.display = 'none';
         document.getElementById('loginNavItem').style.display = 'block';
+        document.getElementById('logoutNavItem').style.display = 'none';
     }
 
 });
